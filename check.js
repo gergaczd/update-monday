916b2fbb2b89cec32f4fd6ea51eea4b610f33ad7
gergaczd <gergaczdaniel@gmail.com>
2018-11-26 21:31:19 +0100

    feat(flags): use yargs descriptive mode to define flags and extend it with open changelog flag

--- check.js
+++ check.js
@@ -4,9 +4,9 @@
 const checkProject = require('./lib/check-project');
 const { updatePackages } = require('./lib/modules/package-json');
 const command = require('./lib/modules/command');
+const { resolveFlags } = require('./lib/modules/flags/cli-flags');
 
-const flags = require('./lib/modules/cli-flags');
-const { folders, install } = flags.check();
+const { folders, install } = resolveFlags();
 
 (async () => {
   for (let folder of folders) {
