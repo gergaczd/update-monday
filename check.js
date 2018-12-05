05a137d2bf2c3b840815d58faa8e4d02a5b8a640
gergaczd <gergaczdaniel@gmail.com>
2018-12-05 21:32:49 +0100

    perf(changelog): paralell changelog url search fix(package-json): fix reading in package json using via binary command refactor(output): make the output more readable

--- check.js
+++ check.js
@@ -4,15 +4,30 @@
 const checkProject = require('./lib/check-project');
 const { updatePackages } = require('./lib/modules/package-json');
 const command = require('./lib/modules/command');
-const { resolveFlags } = require('./lib/modules/flags/cli-flags');
+const { resolveFlags } = require('./lib/modules/cli-flags');
+const { uniqWith, omit } = require('lodash');
+const { isDeepStrictEqual } = require('util');
 
 const { folders, install } = resolveFlags();
 
 (async () => {
-  for (let folder of folders) {
-    const packagesToUpdate = await checkProject(folder);
-    await updatePackages(folder, packagesToUpdate);
+  try {
+    let packageInformations = [];
+    for (let folder of folders) {
+      const packages = await checkProject(folder);
+      await updatePackages(folder, packages);
 
-    install && await command.installPackages(folder);
+      install && await command.installPackages(folder);
+
+      packageInformations = uniqWith(packageInformations.concat(packages), (first, second) => {
+        return isDeepStrictEqual(omit(first, ['update']), omit(second, ['update']));
+      });
+
+      console.table(packages);
+    }
+  } catch (error) {
+    console.log(error.name + ': ' + error.message);
+    console.log(error.stack);
+    console.log('exiting...');
   }
 })();
