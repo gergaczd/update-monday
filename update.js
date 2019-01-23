157b646e1ac827b8a7736f6688a24e6d0328c7c8
gergaczd <gergaczdaniel@gmail.com>
2019-01-23 22:33:32 +0100

    fix(update-notifier): add the update-notifier to the main um command script too

--- update.js
+++ update.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
 const updateProject = require('./modules/update-project');
 const flags = require('./lib/parse-cli')();
 
@@ -9,9 +11,13 @@ const flags = require('./lib/parse-cli')();
     for (let folder of flags.folders) {
       await updateProject(folder, flags);
     }
+
+    updateNotifier({pkg}).notify();
+    process.exit(0);
   } catch (error) {
     console.log(error.name + ': ' + error.message);
     console.log(error.stack);
     console.log('exiting...');
+    process.exit(0);
   }
 })();
